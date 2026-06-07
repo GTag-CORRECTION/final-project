00d6435ba236c68e615007777cf27d93459ff219
Miguel <muitiregivenm@gamil.com>
2026-06-07 14:15:45 -0500

    day3-update-1

---
+++ ext-styling/search-data.js
@@ -0,0 +1,20 @@
+const pages = [
+  {
+    title: "Home",
+    url: "index.html",
+    content: "badminton rules equipment history welcome homepage",
+    keywords: "badminton rules equipment history welcome homepage",
+  },
+  {
+    title: "Rules",
+    url: "rules.html",
+    content: "badminton scoring rules faults serves doubles singles",
+    keywords: "badminton scoring rules faults serves doubles singles",
+  },
+  {
+    title: "Equipment",
+    url: "equipment.html",
+    content: "racket shuttlecock net shoes gear",
+    keywords: "racket shuttlecock net shoes gear",
+  },
+];
